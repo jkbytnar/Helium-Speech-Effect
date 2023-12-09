c7ca07671b3e1340b70239a822cad92c691ce3b8
Wojtekko <98090683+Wojtekko@users.noreply.github.com>
2023-12-09 19:00:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import analyze
+
+if __name__ == '__main__':
+
+    get_spec = False
+    get_mfcc = True
+    data = analyze.comparison(audio_path="Samogloski", get_spec=get_spec, get_mfcc=get_mfcc)
+
+
